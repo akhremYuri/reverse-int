b672af8f9ab4acd3dc7e6b3edbab80a30fc3abc2
akhremYuri <akhremr@gmail.com>
2021-03-12 11:45:56 +0300

    add function reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let num = "" + n;
+    let arr = num.split("");
+    let arr1 = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] != "-") {
+            arr1.push(arr[i]);
+        }
+    }
+    return arr1.join("");
+};
